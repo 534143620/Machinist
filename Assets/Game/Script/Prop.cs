274876caba40ebd4f2d249a6c09b25138275a501
LAPTOP-ES2SFMS2\Han <534143620@qq.com>
2023-02-06 16:40:46 +0800

    【提交】硬币道具的完成

--- Assets/Game/Script/Prop.cs
+++ Assets/Game/Script/Prop.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class Prop : MonoBehaviour
 {
     public enum PropType
@@ -11,11 +11,21 @@ public class Prop : MonoBehaviour
 
     public PropType Type;
     public int Value = 20;
+    public ParticleSystem  CollectedVFX;
+
+    private void Start() {
+        if(Type == PropType.Coin){
+           transform.DORotate(new Vector3(0, 360, 0), 2f, RotateMode.WorldAxisAdd).SetLoops(-1,LoopType.Restart);
+        }
+    }
 
     private void OnTriggerEnter(Collider other) {
         if(other.tag == "Player")
         {
             other.gameObject.GetComponent<Character>().PickUpItem(this);
+            if(CollectedVFX != null)
+                Instantiate(CollectedVFX,transform.position,Quaternion.identity);
+            transform.DOKill();
             Destroy(gameObject);
         }
     }
